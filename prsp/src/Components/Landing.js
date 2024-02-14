75b6a0322f5e5e03742ea4002ff474e955a227ac
Parminder Singh <113473052+paramsingh77@users.noreply.github.com>
2024-02-14 16:47:07 -0600

    updated commit

--- prsp/src/Components/Landing.js
+++ prsp/src/Components/Landing.js
@@ -2,16 +2,20 @@ import React from 'react'
 import './Landing.css'
 import Projcards from './Projcards'
 const Landing = () => {
+
   return (
     <div className='landing-page'>
       
         <div className='work-progress'>
+          <div className='sideBar'>
+              this is side bar
+          </div>
         <div className='icons'><i class="fa-solid fa-bars"></i></div>
             <div className='text-proj'>
                project <span className='color-space'>space</span>
             </div>
             <div className='icons-right'><a href='https://www.github.com/paramsingh77' target='_blank' rel="noopener noreferrer">Github</a></div>
-            <div className='icons-right'><a href='https://www.linkedin.com/in/psingh20' target="_blank"rel="noopener noreferrer">Linkedin</a></div>
+            <div className='icons-right'><a href='https://www.linkedin.com/in/psingh20' target="_blank"rel="noopener noreferrer">LinkedIn</a></div>
         </div>
         <Projcards/>
     </div>
---
+++ prsp/src/Components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className='style'>
+        This is side bar
+    </div>
+  )
+}
+
+export default Sidebar
